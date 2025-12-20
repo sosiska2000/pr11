d5f68de366e00a136aca109d896fe7a2910c9692
Savelyi <skomor1n@mail.ru>
2025-12-20 22:03:40 +0500

    Разработанный метод для получения токена пользователя.Komornyi

---
+++ APIGigaChatImage/Models/Response/ResponseToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGigaChatImage.Models.Response
+{
+    public class ResponseToken
+    {
+        public string access_token { get; set; }
+        public string expires_at { get; set; }
+    }
+}
--- APIGigaChatImage/Program.cs
+++ APIGigaChatImage/Program.cs
@@ -1,15 +1,57 @@
-﻿using System;
+﻿using APIGigaChatImage.Models.Response;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace APIGigaChatImage
 {
-    internal class Program
+    public class Program
     {
+        public static string ClientId = "";
+        public static string AutorizationKey = "";
         static void Main(string[] args)
         {
+        }
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string ReturnToken = null;
+            string Url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
+
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, sert, chain, SslPolicyErrors) => true;
+
+                using (HttpClient Client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer {bearer}");
+
+                    var Data = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+                    };
+
+                    Request.Content = new FormUrlEncodedContent(Data);
+
+                    HttpResponseMessage Response = await Client.SendAsync(Request);
+
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
+            return ReturnToken;
         }
     }
 }
